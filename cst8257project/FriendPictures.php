2e0efd60286e0ff10f71becdd124bb09080365d4
Medhat <elsa0099@algonquinlive.com>
2024-11-26 15:23:30 -0500

    Add authentication to unfinished pages.

--- cst8257project/FriendPictures.php
+++ cst8257project/FriendPictures.php
@@ -1,3 +1,26 @@
-<?php include("./common/header.php"); ?>
-    
-<?php include('./common/footer.php'); ?>
+<?php
+include_once 'EntityClassLib.php';
+include_once 'Functions.php';
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['user'])) {
+    $_SESSION['redirect_url'] = basename($_SERVER['PHP_SELF']);
+    header("Location: Login.php");
+    exit();
+} else {
+    $user = $_SESSION['user'];
+}
+
+include("./common/header.php");
+
+
+
+
+
+
+include('./common/footer.php'); 
+
+?>
